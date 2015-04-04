00aa357ea041a1b5a5b484626cf669170b64d388
Serg <serg-x@list.ru>
2015-04-04 13:06:13 +0300

    13 level

---
+++ test/level12/lesson04/task03/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static int print() {return 1;}
+    public static long print(long l) {return l;}
+    public static char print(char c) {return c;}
+    public static String print(String s) {return "";}
+    public static double print(double d) {return d;}
+    //Напишите тут ваши методы
+}
---
+++ test/level12/lesson12/home07/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.test.level12.lesson12.home07;
+
+/* Fly, Run, Swim для классов Duck, Penguin, Toad
+Есть интерфейсы Fly(летать), Swim(плавать), Run(бегать).
+Добавь эти интерфейсы классам Duck(утка), Penguin(пингвин), Toad(жаба)
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public interface Fly
+    {
+        public void fly();
+    }
+
+    public interface Run
+    {
+        public void run();
+    }
+
+    public interface Swim
+    {
+        public void swim();
+    }
+
+    abstract class Duck implements Fly, Run, Swim
+    {
+
+    }
+
+    abstract class Penguin implements Run, Swim
+    {
+
+    }
+
+    abstract class Toad implements Swim
+    {
+
+    }
+}

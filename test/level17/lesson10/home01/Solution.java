fe95c7689c8cb07c2c05944a108017a2e0f884f9
Serg <serg-x@list.ru>
2015-04-20 17:44:00 +0300

    level 17 lesson 10

--- test/level17/lesson10/home01/Solution.java
+++ test/level17/lesson10/home01/Solution.java
@@ -9,144 +9,144 @@ import java.util.*;
 4. Список будет использоваться нитями, поэтому позаботьтесь, чтобы все методы были синхронизированы.
 */
 
-public class Solution implements List{
+public class Solution implements List<Long>{
     private ArrayList<Long> original = new ArrayList<Long>();
-    @Override
-    public synchronized Object[] toArray()
-    {
-        return new Object[0];
-    }
 
     @Override
-    public synchronized boolean add(Object o)
+    public synchronized int size()
     {
-        return false;
-    }
-
-    @Override
-    public synchronized Object remove(int index)
-    {
-        return null;
-    }
-
-    @Override
-    public synchronized void add(int index, Object element)
-    {
-
-    }
-
-    @Override
-    public synchronized int lastIndexOf(Object o)
-    {
-        return 0;
-    }
-
-    @Override
-    public synchronized boolean remove(Object o)
-    {
-        return false;
-    }
-
-    @Override
-    public synchronized boolean addAll(Collection c)
-    {
-        return false;
-    }
-
-    @Override
-    public synchronized int indexOf(Object o)
-    {
-        return 0;
-    }
-
-    @Override
-    public synchronized boolean containsAll(Collection c)
-    {
-        return false;
+        return original.size();
     }
 
     @Override
     public synchronized boolean isEmpty()
     {
-        return false;
-    }
-
-    @Override
-    public synchronized List subList(int fromIndex, int toIndex)
-    {
-        return null;
-    }
-
-    @Override
-    public synchronized ListIterator listIterator(int index)
-    {
-        return null;
-    }
-
-    @Override
-    public synchronized int size()
-    {
-        return 0;
+        return original.isEmpty();
     }
 
     @Override
     public synchronized boolean contains(Object o)
     {
-        return false;
+        return original.contains(o);
     }
 
     @Override
-    public synchronized Object set(int index, Object element)
+    public synchronized Iterator<Long> iterator()
     {
-        return null;
+        return original.iterator();
     }
 
     @Override
-    public synchronized Iterator iterator()
+    public synchronized Object[] toArray()
     {
-        return null;
+        return original.toArray();
     }
 
     @Override
-    public synchronized boolean retainAll(Collection c)
+    public synchronized <T> T[] toArray(T[] a)
     {
-        return false;
+        return original.toArray(a);
+    }
+
+    @Override
+    public synchronized boolean add(Long aLong)
+    {
+        return original.add(aLong);
+    }
+
+    @Override
+    public synchronized boolean remove(Object o)
+    {
+        return original.remove(o);
+    }
+
+    @Override
+    public synchronized boolean containsAll(Collection<?> c)
+    {
+        return original.containsAll(c);
+    }
+
+    @Override
+    public synchronized boolean addAll(Collection<? extends Long> c)
+    {
+        return original.addAll(c);
+    }
+
+    @Override
+    public synchronized boolean addAll(int index, Collection<? extends Long> c)
+    {
+        return original.addAll(index, c);
+    }
+
+    @Override
+    public synchronized boolean removeAll(Collection<?> c)
+    {
+        return original.removeAll(c);
+    }
+
+    @Override
+    public synchronized boolean retainAll(Collection<?> c)
+    {
+        return original.retainAll(c);
     }
 
     @Override
     public synchronized void clear()
     {
-
+        original.clear();
     }
 
     @Override
-    public synchronized Object get(int index)
+    public synchronized Long get(int index)
     {
-        return null;
+        return original.get(index);
     }
 
     @Override
-    public synchronized boolean addAll(int index, Collection c)
+    public synchronized void add(int index, Long element)
     {
-        return false;
+        original.add(index, element);
     }
 
     @Override
-    public synchronized ListIterator listIterator()
+    public synchronized Long set(int index, Long element)
     {
-        return null;
+        return original.set(index, element);
     }
 
     @Override
-    public Long[] toArray(Object[] a)
+    public synchronized Long remove(int index)
     {
-        return new Long[0];
+        return original.remove(index);
     }
 
     @Override
-    public synchronized boolean removeAll(Collection c)
+    public synchronized int indexOf(Object o)
     {
-        return false;
+        return original.indexOf(o);
     }
 
+    @Override
+    public synchronized int lastIndexOf(Object o)
+    {
+        return original.lastIndexOf(o);
+    }
+
+    @Override
+    public synchronized ListIterator<Long> listIterator()
+    {
+        return original.listIterator();
+    }
+
+    @Override
+    public synchronized ListIterator<Long> listIterator(int index)
+    {
+        return original.listIterator(index);
+    }
+
+    @Override
+    public synchronized List<Long> subList(int fromIndex, int toIndex)
+    {
+        return original.subList(fromIndex, toIndex);
+    }
 }
---
+++ test/level17/lesson10/home02/Beach.java
@@ -0,0 +1,52 @@
+package com.javarush.test.level17.lesson10.home02;
+
+/* Comparable
+Реализуйте интерфейс Comparable<Beach> в классе Beach, который будет использоваться нитями.
+*/
+
+public class Beach implements Comparable<Beach> {
+    private String name;      //название
+    private float distance;   //расстояние
+    private int quality;    //качество
+
+    public Beach(String name, float distance, int quality) {
+        this.name = name;
+        this.distance = distance;
+        this.quality = quality;
+    }
+
+    public synchronized String getName() {
+        return name;
+    }
+
+    public synchronized void setName(String name) {
+        this.name = name;
+    }
+
+    public synchronized float getDistance() {
+        return distance;
+    }
+
+    public synchronized void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public synchronized int getQuality() {
+        return quality;
+    }
+
+    public synchronized void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    @Override
+    public synchronized int compareTo(Beach o)
+    {
+        int namediff = name.compareTo(o.name);
+        int distdiff = (int) (this.distance - o.distance);
+        int qualdiff = this.quality - o.quality;
+        int diff = namediff - distdiff - qualdiff*1000;
+        return diff;
+    }
+
+}

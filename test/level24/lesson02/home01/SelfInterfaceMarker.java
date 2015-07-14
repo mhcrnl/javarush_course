bac9aec337f172189da8f6f1b27bb510e87dd37f
Serg <serg-x@list.ru>
2015-07-14 14:29:16 +0300

    level 24 lesson 6]

---
+++ test/level24/lesson02/home01/SelfInterfaceMarker.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by User on 13.07.15.
+ */
+public interface SelfInterfaceMarker
+{
+}
---
+++ test/level24/lesson02/home01/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by User on 13.07.15.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker
+{
+    public void move() {
+
+    }
+    public void stop() {
+
+    }
+
+}
